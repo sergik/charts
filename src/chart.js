506cd8a49d1f90098aa2fe7ef0498dc800077968
Volkovich Sergey <volkovich@targetprocess.com>
2019-03-12 15:27:31 +0300

    Draw simple line

--- src/chart.js
+++ src/chart.js
@@ -1,10 +1,32 @@
+import './chart.css'
+let uuid = 0;
+const next_uuid = () => ++uuid
 export class Chart {
-    constructor(data, container) {
-        this.data = data;
-        this.container = container;
-    }
+  constructor(data, container) {
+    this.data = data;
+    this.container = container;
+    this.uuid = next_uuid()
+  }
 
-    draw() {
-        this.container.innerHTML = 'some chart !'
-    }
+  _appendRootElement() {
+      const rootElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+      rootElement.setAttribute('width', this.container.clientWidth)
+      rootElement.setAttribute('height', this.container.clientHeight)
+      rootElement.setAttribute('viewBox', `0 0 ${this.container.clientWidth} ${this.container.clientHeight}`)
+      rootElement.setAttribute('id', `svg__${this.uuid}`)
+      this.container.appendChild(rootElement)
+      return rootElement;
+  }
+
+  draw() {
+    this.rootElement = this._appendRootElement()
+    var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+    newLine.setAttribute('id','line2')
+    newLine.setAttribute('x1','0')
+    newLine.setAttribute('y1','0')
+    newLine.setAttribute('x2','500')
+    newLine.setAttribute('y2','500')
+    newLine.setAttribute("stroke", "black")
+    this.rootElement.appendChild(newLine)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-
+import './index.css'
+import data from './data/chart_data.json'
 import { Chart } from './chart'
 const elements = document.getElementsByClassName('container')
 const container = elements[0]
 
 const chart = new Chart([], container)
-chart.draw()
+chart.draw(data[0])
